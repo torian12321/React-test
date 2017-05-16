199678c1a3ede710cb34e1a56efc863aa39b3367
Aitor palomares <aitor.palomares@hmhco.com>
2017-05-16 17:05:33 +0100

    webpack with multiple configurations

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,98 @@
+const
+webpack = require('webpack'),
+merge   = require('webpack-merge'),
+path    = require('path');
+
+const
+common = {
+  entry  : './app/app',
+  resolve: {
+    //root: __dirname,
+    alias: {
+      Main  : path.resolve(__dirname, './app/components/Main'),
+      Nav   : path.resolve(__dirname, './app/components/Nav'),
+
+      // UI
+      Modal      : path.resolve(__dirname, 'app/components/Modal'),
+      Loader     : path.resolve(__dirname, 'app/components/Loader'),
+      Link       : path.resolve(__dirname, 'app/components/Link'),
+      Rating     : path.resolve(__dirname, 'app/components/Rating'),
+      ProgressBar: path.resolve(__dirname, 'app/components/ProgressBar'),
+      Panel      : path.resolve(__dirname, 'app/components/Panel'),
+      Quote      : path.resolve(__dirname, 'app/components/Quote'),
+      Accordion  : path.resolve(__dirname, 'app/components/Accordion'),
+      Avatar     : path.resolve(__dirname, 'app/components/Avatar'),
+      Gallery    : path.resolve(__dirname, 'app/components/Gallery'),
+      Img        : path.resolve(__dirname, 'app/components/Img'),
+      Tabs       : path.resolve(__dirname, 'app/components/Tabs'),
+      Icon       : path.resolve(__dirname, 'app/components/Icon'),
+      
+      // Form
+      Radio      : path.resolve(__dirname, 'app/components/form/Radio'),
+      Checkbox   : path.resolve(__dirname, 'app/components/form/Checkbox'),
+      Input      : path.resolve(__dirname, 'app/components/form/Input'),
+      Textarea   : path.resolve(__dirname, 'app/components/form/Textarea'),
+      Switch     : path.resolve(__dirname, 'app/components/form/Switch'),
+      Button     : path.resolve(__dirname, 'app/components/form/Button')
+    },
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      { test: /\.(jsx|js)$/, exclude: /(node_modules|bower_components)/, loader: 'babel-loader', query: {presets: ['react', 'es2015', 'stage-1']}},
+      { test: /\.(woff|woff2)$/, loader:'url-loader?prefix=font/&limit=5000' },
+      { test: /\.(ttf|otf)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml' },
+      { test: /\.json$/, loader: 'json-loader' },
+      { test: /\.(mp3|wav)$/, loader: 'url-loader?limit=1' },
+      { test: /\.(png|jpg)$/, loader: 'url-loader?limit=25000' },
+    ],
+  }
+},
+
+
+export_default = merge(common, {
+  output: {
+    path    : __dirname,
+    filename: './public/bundle.js'
+  }
+}),
+
+
+export_production = merge(common, {
+  output: {
+    path    : __dirname,
+    filename: './dist/bundle.js'
+  },
+  plugins: [
+      new webpack.LoaderOptionsPlugin({
+        minimize: true,
+        debug: false
+      }),
+      new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': JSON.stringify('production')
+        }
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        beautify: false,
+        mangle: {
+          screw_ie8: true,
+          keep_fnames: true
+        },
+        compress: {
+          screw_ie8: true
+        },
+        comments: false
+      })
+    ]
+});
+
+
 module.exports = function(env) {
-  return require(`./webpack.${env}.js`)
+  if(env === 'prod'){
+    return export_production;
+  }else{
+    return export_default;
+  }
 }
