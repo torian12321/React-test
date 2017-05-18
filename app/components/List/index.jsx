c5e01e0ae58e11918126702f16ee931d250b66a3
Aitor palomares <aitor.palomares@hmhco.com>
2017-05-18 16:28:42 +0100

    list

---
+++ app/components/List/index.jsx
@@ -0,0 +1,31 @@
+var
+React     = require('react'),
+PropTypes = require('prop-types'),
+classNames= require('classnames');
+
+import Ul from './Ul';
+import Wrapper from './Wrapper';
+
+function List(props) {
+  const ComponentToRender = props.component;
+  let content = (<div></div>);
+
+  // If we have items, render them
+  if (props.items) {
+    content = props.items.map((item, index) => (
+      <ComponentToRender key={`item-${index}`} item={item} />
+    ));
+  } else {
+    // Otherwise render a single component
+    content = (<ComponentToRender />);
+  }
+
+  return <ul>{content}</ul>;
+}
+
+List.propTypes = {
+  component: React.PropTypes.func.isRequired,
+  items: React.PropTypes.array,
+};
+
+export default List;
