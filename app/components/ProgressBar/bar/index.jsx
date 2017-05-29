98c170b37984316aa041f1785614fdf0c79630e9
Aitor palomares <aitor.palomares@hmhco.com>
2017-05-29 16:01:16 +0100

    progressbar parent manage classname building

--- app/components/ProgressBar/bar/index.jsx
+++ app/components/ProgressBar/bar/index.jsx
@@ -5,12 +5,7 @@ classNames= require('classnames'),
 styles    = require('./_style.less');
 
 const ProgressBar = (props) =>(
-    <div className={classNames(
-        'progressBar',
-        props.style,
-        props.alignment ? 'pb-' + props.alignment : null,
-        props.color
-    )}>
+    <div className={props.className}>
         <div className="bar" style={{ width: props.val + '%' }}>
             {props.children}
             {props.label ? <label>{props.label}</label> : null}
@@ -23,9 +18,7 @@ ProgressBar.propTypes = {
     children : PropTypes.string,
     label    : PropTypes.string,
     val      : PropTypes.number.isRequired,
-    style    : PropTypes.oneOf(['underline', 'bordered']),
-    alignment: PropTypes.oneOf(['left', 'right']),
-    color    : PropTypes.oneOf(['main', 'sec', 'info', 'success', 'danger', 'warning'])
+    className: PropTypes.string
 };
 
 module.exports = ProgressBar;
