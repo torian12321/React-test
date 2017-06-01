3174232d92aecd2bd670d08bf40a299c16a5dd81
Aitor palomares <aitor.palomares@hmhco.com>
2017-06-01 12:31:39 +0100

    loading options

--- app/components/Loading/Readme.md
+++ app/components/Loading/Readme.md
@@ -2,3 +2,26 @@ Basic
 ```
 <Loading />
 ```
+
+Spinner
+```
+<Loading type='pulse' />
+```
+
+Sizes
+```
+<div>
+    <Loading size='xs' />
+    <Loading size='sm' />
+    <Loading size='md' />
+    <Loading size='lg' />
+</div>
+```
+
+Sizes
+```
+<div>
+    <Button><Loading size='xs' /></Button>
+    <Message><Loading /></Message>
+</div>
+```
--- app/components/Loading/index.jsx
+++ app/components/Loading/index.jsx
@@ -1,9 +1,20 @@
 var
-React = require('react'),
-styles= require('./_style.less');
+React     = require('react'),
+PropTypes = require('prop-types'),
+classNames= require('classnames'),
+styles    = require('./_style.less');
 
 const Loading = props => (
-    <div className="loading" />
+    <div className={classNames('loading', props.type, props.size)} />
 );
+
+
+Loading.propTypes = {
+    type: PropTypes.oneOf(['spinner', 'pulse']),
+    size: PropTypes.oneOf(['xs', 'sm', 'md', 'lg'])
+};
+Loading.defaultProps = {
+    type: 'spinner'
+};
 
 module.exports = Loading;
