ed6a840ba664d762c4cc7ae9761f7954232597a8
Aitor palomares <aitor.palomares@hmhco.com>
2017-06-07 10:22:28 +0100

    modal

--- app/components/Modal/modal.jsx
+++ app/components/Modal/modal.jsx
@@ -5,16 +5,42 @@ classNames= require('classnames'),
 styles    = require('./_style.less');
 
 
-const Modal = props => (props.isVisible ? (
-    <div className={classNames('modal', props.className)}>
-        <div className='modal-bg' />
-        <div className='modal-container'>
-            <div className='modal-content'>
-                {props.children}
+class Modal extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isVisible: this.props.isVisible
+        };
+
+        this.handleClose = this.handleClose.bind(this);
+    }
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.isVisible !== this.state.isVisible) {
+            this.setState({isVisible: nextProps.isVisible});
+        }
+    }
+
+    handleClose(){
+        this.setState({
+            isVisible: false
+        });
+    }
+    
+    render(){
+        return (this.state.isVisible ? (
+            <div className={classNames('modal', this.props.className)}>
+                <div className='modal-bg' />
+                <div className='modal-container'>
+                    <div className='modal-close' onClick={this.handleClose}>X</div>
+                    <div className='modal-content'>
+                        
+                        {this.props.children}
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-) : null);
+        ) : null)
+    }
+};
 
 Modal.propTypes = {
     children : PropTypes.node.isRequired,
