5e530394665abb3a87c57ee1b28bc8875c6ff437
Aitor palomares <aitor.palomares@hmhco.com>
2017-06-25 22:07:26 +0100

    table style

--- app/components/Table/index.jsx
+++ app/components/Table/index.jsx
@@ -5,14 +5,25 @@ classNames= require('classnames'),
 styles    = require('./_style.less');
 
 const Table = props => (
-    <table className={classNames('table', props.type, props.size)}>
+    <table className={classNames(
+        'table',
+        props.className,
+        {"striped"  : props.striped},
+        {"bordered" : props.bordered}
+    )}>
         {props.children}
     </table>
 );
 
 Table.propTypes = {
     children : PropTypes.node.isRequired,
-    className: PropTypes.string
+    className: PropTypes.string,
+    striped  : PropTypes.string.bool,
+    bordered : PropTypes.string.bool
+};
+Table.defaultProps = {
+    striped : true,
+    bordered: false
 };
 
 module.exports = Table;
