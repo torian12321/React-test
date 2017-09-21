5e2e5ada894704c39012b89016daaa155b29d831
Aitor palomares <aitor.palomares@hmhco.com>
2017-09-21 12:52:52 +0100

    css corrections on avatar

--- src/components/Avatar/Readme.md
+++ src/components/Avatar/Readme.md
@@ -3,16 +3,27 @@
 <Avatar />
 ```
 
+#### Custom src
+Use a local image or from a url
+```
+const imgExample = require ('./example.jpg');
+
+<div>
+  <Avatar src='http://placeimg.com/200/200/people' />
+  <Avatar src={imgExample} />
+</div>
+```
+
 #### States
 May be used for 'online', 'in a meeting' etc.
 ```
 <div>
 	<Avatar state='main'/>
-    <Avatar state='sec'/>
-    <Avatar state='success'/>
-    <Avatar state='danger'/>
-    <Avatar state='warning'/>
-    <Avatar state='disable'/>
+  <Avatar state='sec'/>
+  <Avatar state='success'/>
+  <Avatar state='danger'/>
+  <Avatar state='warning'/>
+  <Avatar state='disable'/>
 </div>
 ```
 
--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -1,31 +1,30 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React      from 'react';
+import PropTypes  from 'prop-types';
 import classNames from 'classnames';
-import styles from './_style';
-import ImgDef from '../../img/default/user.png';
+import styles     from './_style';
+import ImgDef     from '../../img/default/user.png';
 
 const Avatar = props => (
-    <div 
-        style    ={{backgroundImage: `url(${props.src})`}}
-        className={classNames(
-            'avatar',
-            props.size,
-            props.state,
-            props.className
-        )}
-    />
+  <div 
+      style    ={{backgroundImage: `url(${props.src})`}}
+      className={classNames(
+        'avatar',
+        props.size,
+        props.state,
+        props.className
+      )}
+  />
 );
 
 
 Avatar.propTypes = {
-    src      : PropTypes.string.isRequired,
-    size     : PropTypes.oneOf(['xs', 'sm', 'md', 'lg']),
-    state    : PropTypes.oneOf(['main', 'sec', 'success', 'danger', 'warning', 'disable']),
-    className: PropTypes.string
+  src      : PropTypes.string.isRequired,
+  size     : PropTypes.oneOf(['xs', 'sm', 'md', 'lg']),
+  state    : PropTypes.oneOf(['main', 'sec', 'success', 'danger', 'warning', 'disable']),
+  className: PropTypes.string
 };
 Avatar.defaultProps = {
-    //src: 'http://placeimg.com/200/200/people'
-    src: ImgDef
+  src: ImgDef
 };
 
 module.exports = Avatar;
