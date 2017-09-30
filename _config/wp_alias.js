7624d97d726a760ee644d1982ebcf8c3653dc747
torian <torianeleterno@hotmail.com>
2017-09-30 09:30:52 +0100

    new less files and structure

--- _config/wp_alias.js
+++ _config/wp_alias.js
@@ -6,6 +6,6 @@ module.exports = {
   // // Not necessary unless you consume a module using `createClass`
   // 'create-react-class': 'preact-compat/lib/create-react-class',
 
-  LESS_conf: path.resolve(__dirname, '../src/styles'),
+  LESS_conf: path.resolve(__dirname, '../src/styles/conf'),
   export   : path.resolve(__dirname, '../export')
 };
