6db214f8146a405e21ed9f3fddcca65c5400993c
Aitor palomares <aitor.palomares@hmhco.com>
2017-11-24 11:45:25 +0000

    SASS with BEM

--- src/components/Quote/index.jsx
+++ src/components/Quote/index.jsx
@@ -2,26 +2,29 @@ import React      from 'react';
 import PropTypes  from 'prop-types';
 import classNames from 'classnames';
 import Icon       from '../Icon';
-import './_style.less';
+import './_style.scss';
 
 class Quote extends React.PureComponent {
   render(){
     return(
       <blockquote className={classNames('quote', this.props.className)} cite={this.props.cite}>
         {this.props.children}
-        {this.props.author ? <span className="quote-author">{ this.props.author }</span> : null}
-        <Icon className="icon-open"  name="quote" />
-        <Icon className="icon-close" name="quote" rotate={180} />
+        {this.props.author ?
+          <span className="quote__author">{ this.props.author }</span> :
+          null
+        }
+        <Icon className="quote__icon quote__icon--open"  name="quote" />
+        <Icon className="quote__icon quote__icon--close" name="quote" rotate={180} />
       </blockquote>
     );
   }
 }
 
 Quote.propTypes = {
-    children : PropTypes.string.isRequired,
-    className: PropTypes.string,
-    author   : PropTypes.string,
-    cite     : PropTypes.string
+  children : PropTypes.string.isRequired,
+  className: PropTypes.string,
+  author   : PropTypes.string,
+  cite     : PropTypes.string
 };
 
 module.exports = Quote;
