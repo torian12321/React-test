abcc06e5e3ad66d5ff58807c93250f01408b662b
Aitor palomares <aitor.palomares@hmhco.com>
2017-12-06 14:47:48 +0000

    show version on styleguide

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,27 +1,27 @@
-//const webpack = require('./webpack.config');
-const path = require('path');
-const wp_common   = require('./_config/wp_common');
+const path      = require('path');
+const pkg       = require('./package.json');
+const wp_common = require('./_config/wp_common');
 
 module.exports = {
-	title         : 'Components Styleguide',
-	template      : './styleguide/template.html',
-	serverPort    : 6060,
-	defaultExample: false,
-	showUsage     : false,
-	showCode      : false,
-	highlightTheme: 'material',
-	skipComponentsWithoutExample: true,
-	webpackConfig : wp_common,
+  title         : `Components Styleguide v${pkg.version}`,
+  template      : './styleguide/template.html',
+  serverPort    : 6060,
+  defaultExample: false,
+  showUsage     : false,
+  showCode      : false,
+  showSidebar   : true,
+  highlightTheme: 'material',
+  skipComponentsWithoutExample: true,
+  webpackConfig : wp_common,
 	sections: [
-		{ name: 'Grid'		, components: 'src/components/_grid/**/index.jsx' },
-		{ name: 'Components', components: 'src/components/*/index.jsx' },
-		{ name: 'Form'      , components: 'src/components/_form/**/index.jsx' },
-		{ name: 'Others'    , components: 'src/components/_others/**/index.jsx' },
+    { name: 'Grid'		, components: 'src/components/_grid/**/index.jsx' },
+    { name: 'Components', components: 'src/components/*/index.jsx' },
+    { name: 'Form'      , components: 'src/components/_form/**/index.jsx' },
+    { name: 'Others'    , components: 'src/components/_others/**/index.jsx' },
 	],
-    getComponentPathLine: function (componentPath) {
-		// Use folder name as import reference
-		var dir = path.dirname(componentPath).split('/');
-
-        return `import { ${dir[dir.length-1]} } from 'react-test';`;
+  getComponentPathLine: function (componentPath) {
+    // Use folder name as import reference
+    var dir = path.dirname(componentPath).split('/');
+    return `import { ${dir[dir.length-1]} } from 'react-test';`;
 	},
 };
