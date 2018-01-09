a327f112c0825af21cdeed197b92582cd9809892
Aitor palomares <aitor.palomares@hmhco.com>
2018-01-09 09:14:23 +0000

    inform when image alt is missed

--- src/components/Img/index.jsx
+++ src/components/Img/index.jsx
@@ -4,25 +4,39 @@ import ImgDef     from '../../img/default/img.png';
 
 class Img extends React.PureComponent {
   render() {
-    const { src, alt, ...other } = this.props;
+    const { src, alt, presentational, ...other } = this.props;
 
     return (
       <img
         {...other}
         src ={src}
         alt ={alt}
-        role={!alt && 'presentation'}
+        role={presentational ? 'presentation' : null}
       />
     )
   }
 }
 
 Img.propTypes = {
-  src: PropTypes.string.isRequired,
-  alt: PropTypes.string
+  src           : PropTypes.string.isRequired,
+  presentational: PropTypes.bool,
+  alt           : function(props, propName, componentName) {
+    let
+    errorMessage = null,
+    alt          = props[propName];
+
+    if (!alt && !props.presentational) {
+      errorMessage = 'Missing `'+ propName +'` for image ' + props.src;
+    } else if ((alt && !(typeof alt === 'string' || alt instanceof String))) {
+      errorMessage = 'Invalid prop `'+ propName +'` of type `'+ (typeof alt) +'` supplied to `'+ componentName +'`, expected `string`.';
+    }
+
+    return errorMessage && new Error(errorMessage);
+  }
 };
 Img.defaultProps = {
-  src: ImgDef
+  src           : ImgDef,
+  presentational: false
 };
 
 module.exports = Img;
